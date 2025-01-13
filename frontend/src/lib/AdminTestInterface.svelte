4956fd9af94ffed9641e0d7e365fea543ea09dea
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-01-13 11:56:22 +0100

    Added comments and streamlined code

--- frontend/src/lib/AdminTestInterface.svelte
+++ frontend/src/lib/AdminTestInterface.svelte
@@ -1,140 +1,272 @@
 <script lang="ts">
-    
-    import SvelteTable from 'svelte-table';
-    import { Heart, Trash, Check } from 'svelte-iconoir';
+    import SvelteTable from "svelte-table";
+    import { Heart, Trash, Check } from "svelte-iconoir";
 
-    import { joinQueue, getMetrics, leaveQueue, confirmConnection } from './queue';
-    import type { UserRequest } from './types';
+    import {
+        joinQueue,
+        getMetrics,
+        leaveQueue,
+        confirmConnection,
+    } from "./queue";
+    import type { UserRequest } from "./types";
 
-    let users = [];
+    /**
+     * Represents the simulated user's data in the queue.
+     * @remarks
+     * This is a temporary solution to be finalized or replaced by QueueStatus in types.ts
+     * 
+     * @param id - The user's unique identifier.
+     * @param status - The user's current status in the queue. waiting, draft, connected, or disconnected.
+     * @param position - The user's position in the queue.
+     * @param timeSinceJoin - The time in seconds since the user joined the queue.
+     * @param draftTimer - The time in seconds since the user entered the draft.
+     * @param sessionTimer - The time in seconds since the user entered the session.
+     * 
+     */
+    interface UserQueueStats {
+        id: string;
+        status: string;
+        position: number;
+        timeSinceJoin: number;
+        draftTimer: number;
+        sessionTimer: number;
+    }
 
+    let users: UserQueueStats[] = [];
+
+    // Svelte-table columns
     const columns = [
-        { title: 'ID', key: 'id', value: v => v.id },
-        { title: 'Status', key: 'status', value: v => v.status },
-        { title: 'Position', key: 'position', value: v => v.position },
-        { title: 'Time since join', key: 'timeSinceJoin', value: v => v.timeSinceJoin },
-        { title: 'Draft timer', key: 'draftTimer', value: v => v.draftTimer },
-        { title: 'Session timer', key: 'sessionTimer', value: v => v.sessionTimer },
-        { title: 'Heartbeat', key: 'heartbeat', renderComponent: Heart},
-        { title: 'Leave', key: 'leave', renderComponent: Trash},
-        { title: 'Confirm', key: 'confirm', renderComponent: Check}
+        {
+            title: "ID",
+            key: "id",
+            value: (v: UserQueueStats) => v.id,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Status",
+            key: "status",
+            value: (v: UserQueueStats) => v.status,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Position",
+            key: "position",
+            value: (v: UserQueueStats) => v.position,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Time since join",
+            key: "timeSinceJoin",
+            value: (v: UserQueueStats) => v.timeSinceJoin,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Draft timer",
+            key: "draftTimer",
+            value: (v: UserQueueStats) => v.draftTimer,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Session timer",
+            key: "sessionTimer",
+            value: (v: UserQueueStats) => v.sessionTimer,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Heartbeat",
+            key: "heartbeat",
+            renderComponent: Heart,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Leave",
+            key: "leave",
+            renderComponent: Trash,
+            class: "px-4 py-2",
+        },
+        {
+            title: "Confirm",
+            key: "confirm",
+            renderComponent: Check,
+            class: "px-4 py-2",
+        },
     ];
 
-    let queueStatus = 'unknown';
-    let queueLength = 0;
-    let testingLength = 0;
+    // Storing the last response from the server
+    let lastResponse = "";
 
-    let lastResponse = '';
-
+    /**
+     * Generates a random user with default data.
+     * 
+     * @returns A UserRequest object with the generated user's random user_id.
+     * @remarks Also adds the user to the queue list. (Which should not be handled here)
+     */
     function generateRandomUser() {
-        let user: UserRequest = {
-            user_id: Math.random().toString(36).substring(2, 9)
+        let new_user_request: UserRequest = {
+            user_id: Math.random().toString(36).substring(2, 9),
         };
 
         // Temporary solution to test table. This functionnality is supposed to be handled through an endpoint.
-        let new_user = {
-            id: user.user_id,
-            status: 'waiting',
+        let new_user: UserQueueStats = {
+            id: new_user_request.user_id,
+            status: "waiting",
             position: users.length + 1,
             timeSinceJoin: 0,
             draftTimer: 0,
             sessionTimer: 0,
-            heartbeat: 'heartbeat',
-            leave: 'leave'
-        }
+        };
 
         users = [...users, new_user];
-        console.log(users)
-        return user;
+        console.log(users);
+        return new_user_request;
     }
 
+    /**
+     * Adds a randomly generated user to the queue.
+     */
     function handleAddUserToQueue() {
         let user = generateRandomUser();
-        console.log('Adding user to queue');
-        joinQueue(user).then(data => {
-            console.log(data);
-            lastResponse = JSON.stringify(data);
-        }).catch(err => {
-            console.error(err.message);
-            lastResponse = JSON.stringify(err.message);
-        });
+        console.log("Adding user to queue");
+        joinQueue(user)
+            .then((data) => {
+                console.log(data);
+                lastResponse = JSON.stringify(data);
+            })
+            .catch((err) => {
+                console.error(err.message);
+                lastResponse = JSON.stringify(err.message);
+            });
     }
 
+    /**
+     * Gets the global queue metrics from the server.
+     * - active_users
+     * - waiting_users
+     * - total_slots
+     */
     function handleGetMetrics() {
-        console.log('Getting metrics');
-        getMetrics().then(data => {
-            console.log(data);
-            lastResponse = JSON.stringify(data);
-        }).catch(err => {
-            console.error(err.message);
-            lastResponse = JSON.stringify(err.message);
-        });
+        console.log("Getting metrics");
+        getMetrics()
+            .then((data) => {
+                console.log(data);
+                lastResponse = JSON.stringify(data);
+            })
+            .catch((err) => {
+                console.error(err.message);
+                lastResponse = JSON.stringify(err.message);
+            });
     }
 
-
-    function handleClick(e) {
+    /**
+     * Handles the click event on the table cells.
+     * 
+     * @param e - The CustomEvent object containing the cell's details.
+     */
+    function handleClick(e: CustomEvent) {
         let detail = e.detail;
 
-        if (detail.key == 'heartbeat') {
+        if (detail.key == "heartbeat") {
             handleHeartbeat(detail.row);
-        } else if (detail.key == 'leave') {
+        } else if (detail.key == "leave") {
             handleLeaveQueue(detail.row);
-        } else if (detail.key == 'confirm') {
+        } else if (detail.key == "confirm") {
             handleConfirmConnection(detail.row);
         }
-        console.log(e)
+        console.log(e);
     }
 
-    function handleHeartbeat(row) {
-        console.log('Heartbeat for user', row.id);
+    /**
+     * Handles the heartbeat event for a user. (Does nothing for now)
+     * @param row - The user's data.
+     */
+    function handleHeartbeat(row: UserQueueStats) {
+        console.log("Heartbeat for user", row.id);
     }
 
-    function handleLeaveQueue(row) {
-        console.log('Leaving queue for user', row.id);
-        leaveQueue(row.id).then(data => {
-            console.log(data);
-            lastResponse = JSON.stringify(data);
-        }).catch(err => {
-            console.error(err.message);
-            lastResponse = JSON.stringify(err.message);
+    /**
+     * Requests the removal of a user from the queue from the server.
+     * @param row - The user's data.
+     */
+    function handleLeaveQueue(row: UserQueueStats) {
+        console.log("Leaving queue for user", row.id);
+        let cur_user: UserRequest = {
+            user_id: row.id,
+        };
+        leaveQueue(cur_user)
+            .then((data) => {
+                console.log(data);
+                lastResponse = JSON.stringify(data);
+            })
+            .catch((err) => {
+                console.error(err.message);
+                lastResponse = JSON.stringify(err.message);
+            });
+    }
+
+    /**
+     * Requests the confirmation of a connection for a user from the server.
+     * @param row - The user's data.
+     */
+    function handleConfirmConnection(row: UserQueueStats) {
+        console.log("Confirming user", row.id);
+        let cur_user: UserRequest = {
+            user_id: row.id,
+        };
+        confirmConnection(cur_user)
+            .then((data) => {
+                console.log(data);
+                lastResponse = JSON.stringify(data);
+            })
+            .catch((err) => {
+                console.error(err.message);
+                lastResponse = JSON.stringify(err.message);
+            });
+    }
+
+    // Refresh the queue's and its users' status every second 
+    setInterval(() => {
+        refreshUsers();
+    }, 1000);
+
+    function refreshUsers() {
+        // users = handleGetQueueStatus(); // To be implemented once endpoint is ready
+        users = users.map((user) => {
+            user.timeSinceJoin += 1;
+            return user;
         });
     }
-
-    function handleConfirmConnection(row) {
-        console.log('Confirming user', row.id);
-        confirmConnection(row.id).then(data => {
-            console.log(data);
-            lastResponse = JSON.stringify(data);
-        }).catch(err => {
-            console.error(err.message);
-            lastResponse = JSON.stringify(err.message);
-        });
-    }
-
 </script>
 
-<div class="absolute top-0 left-0 right-0 flex flex-col items-center bg-gray-100">
-    <div class="mt-6 relative bg-white pt-10 pb-2 px-2 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg rounded">
+<div
+    class="absolute top-0 left-0 right-0 flex flex-col items-center bg-gray-100"
+>
+    <div
+        class="mt-6 relative bg-white pt-10 pb-2 px-2 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg rounded"
+    >
         <h1 class="text-xl font-bold mb-4 mx-6">Response</h1>
-        <div class="relative bg-black pt-10 pb-8 px-4 ring-1 text-white ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg rounded font-mono overflow-scroll">
+        <div
+            class="relative bg-black pt-10 pb-8 px-4 ring-1 text-white ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg rounded font-mono overflow-scroll"
+        >
             {lastResponse}
         </div>
     </div>
-    <div class="mt-6 relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg  rounded">
+    <div
+        class="mt-6 relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-2 sm:rounded-lg rounded"
+    >
         <h1 class="text-xl font-bold mb-4">Admin</h1>
         <p>Simulate users entering, leaving, and timing out in the queue</p>
         <div class="flex space-x-2 my-4">
-            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={handleAddUserToQueue}>Add to queue</button>
-            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={handleGetMetrics}>Get Metrics</button>
+            <button
+                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                on:click={handleAddUserToQueue}>Add to queue</button
+            >
+            <button
+                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                on:click={handleGetMetrics}>Get Metrics</button
+            >
         </div>
         <div>
-            <SvelteTable 
-                columns="{columns}" 
-                rows="{users}" 
-                on:clickCell="{handleClick}"    
-                class="w-full"
-            />
+            <SvelteTable {columns} rows={users} on:clickCell={handleClick} />
         </div>
     </div>
 </div>
-
